b9987308b4c5dd41c57f477531e3ac1f3c1e0479
Jota Santos <113864204+JotaEse68@users.noreply.github.com>
2026-06-14 09:09:25 +0200

    Add simple Rostizadora page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 'use client';
 
 export default function HomePage() {
-  return <main>La Rostizadora Digital</main>;
+  return (
+    <main className="shell">
+      <section className="hero">
+        <h1>La <em>Rostizadora</em> Digital</h1>
+        <p>README al fuego, soluciones al plato.</p>
+        <img className="mascot" src="/rostizadora-coach.svg" alt="Mascota" />
+      </section>
+    </main>
+  );
 }
